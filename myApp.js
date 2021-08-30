94709070a57290123c3b0872a768058ba89d904b
octdes241295 <88241709+octdes241295@users.noreply.github.com>
2021-08-30 16:22:45 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,61 @@
+
+
+
+
 var express = require('express');
 var app = express();
 console.log("Hello World");
 
+var response="go";
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  response = "Hello World".toUpperCase();
+} else {
+  response = "Hello World";
+};
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: response
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
 
 
 
